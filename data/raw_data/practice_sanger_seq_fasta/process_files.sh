1284ffe2032e3a0f4e8c22aec5a1b742e561a284
Angela Jocson <aljocson@dons.usfca.edu>
2019-11-26 23:05:53 +0000

    Fix script to pass shellcheck

--- data/raw_data/practice_sanger_seq_fasta/process_files.sh
+++ data/raw_data/practice_sanger_seq_fasta/process_files.sh
@@ -12,13 +12,13 @@ echo "Starting for loop..."
 # this is a for loop
 for file in *file*.txt
 do
-	echo $file
+	echo "$file"
 done 
 
 # here's a for loop with numbers
 for number in {1..10}
 do
-	echo Number is $number
+	echo Number is "$number"
 	sleep 1
 	echo The pause is done, moving to the next loop
 	echo -------------
@@ -26,7 +26,6 @@ done
 
 for seqfile in *.fasta
 do
-	echo In $seqfile there are $(grep -c ">" $seqfile) sequences
+	echo In "$seqfile" there are $(grep -c ">" "$seqfile") sequences
 	echo In this directory ther are $(ls *.fasta | wc -l) fasta files
 done
-	
